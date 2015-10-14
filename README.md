84e12d47ea639be3f12dad6bef94275ce6bc1b09
Johnny Hausman <johnny@appdevdesigns.net>
2015-10-14 16:33:27 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+opstool-mobile-manager ![Build Status](https://travis-ci.org/appdevdesigns/opstool-mobile-manager.png)
+================
+
+A new plugin.
---
+++ config/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Routes
+ *
+ * Use this file to add any module specific routes to the main Sails
+ * route object.
+ */
+
+
+module.exports = {
+
+
+  /*
+
+  '/': {
+    view: 'user/signup'
+  },
+  '/': 'opstool-mobile-manager/PluginController.inbox',
+  '/': {
+    controller: 'opstool-mobile-manager/PluginController',
+    action: 'inbox'
+  },
+  'post /signup': 'opstool-mobile-manager/PluginController.signup',
+  'get /*(^.*)' : 'opstool-mobile-manager/PluginController.profile'
+
+  */
+
+
+};
+
